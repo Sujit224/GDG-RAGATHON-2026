c1d6e44d05f13078b464717544084c1fc9d51c71
Suryansh-cell <shauryachhota21@gmail.com>
2026-04-08 23:40:24 +0530

    feat: setup ChromaDB and retrieval chain for insurance queries

---
+++ Statement-1-Insurance-Decoder/src/rag_engine.py
@@ -0,0 +1,28 @@
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain_google_genai import ChatGoogleGenerativeAI
+def setup_rag_chain(documents):
+ # Use Gemini Embeddings
+ embeddings =
+GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+ # Create local Vector Store
+ vectorstore = Chroma.from_documents(
+ documents=documents,
+ embedding=embeddings,
+ persist_directory="./chroma_db"
+ )
+
+ # Initialize LLM (Gemini)
+ llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+temperature=0)
+
+ # Setup Retriever
+ qa_chain = RetrievalQA.from_chain_type(
+ llm=llm,
+ chain_type="stuff",
+ retriever=vectorstore.as_retriever(search_kwargs={"k": 3}),
+ return_source_documents=True
+ )
+ return qa_chain
